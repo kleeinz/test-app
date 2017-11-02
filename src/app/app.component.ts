bba4095623912870e03269c49471bdf40a9dfe94
ilumina <ilumina@22hw035595.SOAM.TCS.com>
2017-11-02 16:43:04 -0600

    Add menu-side

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, NavController, MenuController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-//import { SigninPage } from '../pages/signin/signin';
+import { SigninPage } from '../pages/signin/signin';
 import { ClientsPage } from '../pages/clients/clients';
 import firebase from 'firebase';
 
@@ -11,19 +11,37 @@ import firebase from 'firebase';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  // rootPage:any = SigninPage;
-  rootPage:any = ClientsPage;
+  rootPage:any = SigninPage;
+  clientsPage = ClientsPage;
+  // rootPage:any = ClientsPage;
+  isAuthenticated = false;
+  @ViewChild('nav') nav: NavController;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,
+              private menuCtrl: MenuController) {
     firebase.initializeApp({
       apiKey: "AIzaSyCx7oTapDY2FubRwmP9zK9CTY5YO6pnmNA",
       authDomain: "test-app-d61e5.firebaseapp.com"
     });
+    firebase.auth().onAuthStateChanged(user => {
+      if(user) {
+        this.isAuthenticated = true;
+        this.nav.setRoot(ClientsPage);
+      } else {
+        this.isAuthenticated = false;
+        this.nav.setRoot(SigninPage);
+      }
+    })
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  onLoad(page: any) {
+    this.nav.setRoot(page);
+    this.menuCtrl.close();
   }
 }
