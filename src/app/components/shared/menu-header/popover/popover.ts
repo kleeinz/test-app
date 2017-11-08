bdc8629e36f584a8f25cf0ddb1e6b84f2db16ce2
ilumina <ilumina@22hw035595.SOAM.TCS.com>
2017-11-08 17:05:44 -0600

    Add logout

--- src/app/components/shared/menu-header/popover/popover.ts
+++ src/app/components/shared/menu-header/popover/popover.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { ViewController } from 'ionic-angular';
+import { SigninPage } from '../../../../../pages/signin/signin';
 
 @Component({
   selector: 'page-popover',
   templateUrl: './popover.html'
 })
 export class PopoverPage {
+  signinPage = SigninPage;
   constructor(private viewCtrl: ViewController) {
 
   }
-  
+
 }
